293271e5930d4a28921df5987abe7cf209c596bd
ppap0523 <58874424+ppap0523@users.noreply.github.com>
2020-10-15 13:28:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js');
 const client = new discord.Client();
-const token = 'NzY1Nzg4Njk5MzkyNjA2MjM4.X4Z6TQ.vL251LkDiTo5EBs_Wn7BmrfALls';
+const token = process.env.token;
 
 client.on('ready', () => {
     console.log('on');
